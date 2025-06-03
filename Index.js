926f3977369e5c17a03c78b39d826b6109ca8502
JoshuaTolhuis <j.o.tolhuis@st.hanze.nl>
2025-06-03 14:44:50 +0200

    Added two pug files, one for user profiles and one for the form requests. Also added static css template rendering to the webpages.

--- Index.js
+++ Index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 const PORT = 3000;
 
 const users = require('./routes/users');
 const posts = require('./routes/posts');
+const requests = require('./routes/PhyloForm');
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+app.use(express.static('./public'));
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use('/users', users);
 app.use('/posts', posts);
+app.use('/requests', requests);
 
 app.get('/', (req,res) => {
     res.send("<h1> Welcome to the homepage! </h1>");
